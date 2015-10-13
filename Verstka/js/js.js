69e99c9f183d17a3fc4a3533f3ea8300e4774316
Roman <romanus_n@ukr.net>
2015-10-13 11:54:05 +0300

    decomposition js

--- Verstka/js/js.js
+++ Verstka/js/js.js
@@ -1,14 +1,55 @@
-$(document).ready(function(){
-  $("#owl-demo").owlCarousel({
-      items : 1,
-      autoplay : true,
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      nav:true,
-      navText : false,
-      loop : true,
-      responsive : false, 
-      itemsScaleUp : false, 
-  });
+$(document).ready(function () {
+    $("#owl-demo").owlCarousel({
+        items: 1,
+        autoplay: true,
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        nav: true,
+        navText: false,
+        loop: true,
+        responsive: false,
+        itemsScaleUp: false,
+    });
+
+    function myTags(mytags) {
+        mytags = mytags.replace(/<A/g, '<a')
+            .replace(/\/A>/g, "/a>")
+            .replace(/(\starget=_)(\w*)/g, ' target="_$2"')
+            .replace(/(\sclass=)(?!")(\w*)/g, ' class="$2"')
+            .replace(/(\sname=)(?!")(\w*)/g, ' name="$2"')
+            .replace(/(\sid=)(?!")(\w*)/g, ' id="$2"')
+            .replace(/(\srel=)(?!")(\w*)/g, ' rel="$2"');
+        mytags = encodeURIComponent(mytags).replace(/!/g, '%21')
+            .replace(/'/g, '%27').replace(/\(/g, '%28')
+            .replace(/\)/g, '%29').replace(/\*/g, '%2A');
+        var rnumber = Math.floor(Math.random() * 9999999);
+        var flashvars = {
+            tcolor: "ffffff",
+            tcolor2: "0x000000",
+            hicolor: "0xB12AC8",
+            tspeed: "110",
+            distr: "true",
+            mode: "tags",
+            tagcloud: mytags
+        };
+        var params = {
+            allowScriptAccess: "always",
+            wmode: 'transparent'
+        };
+        var attributes = {
+            id: "flash_cloud"
+        };
+        swfobject.embedSWF("tagcloud.swf?r=" + rnumber,
+            "tags", "490", "300", "9.0.0",
+            "expressInstall.swf", flashvars,
+            params, attributes);
+    }
+
+    window.onload = function () {
+        var mytags = "<tags>"
+            + document.getElementById('tags').innerHTML
+            + "</tags>";
+        myTags(mytags);
+    };
 })
